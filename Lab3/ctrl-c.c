516a59812aa276fee302fb0ff53193952223f40a
Lucca Rocha <lucca.v.rocha@gmail.com>
2024-04-04 19:04:41 -0300

    Lab 3 done

---
+++ Lab3/ctrl-c.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define EVER ;;
+void intHandler(int sinal);
+void quitHandler(int sinal);
+void killhandler(int sinal);
+int main(void)
+{
+	int a = getpid();
+	printf("PID: %d\n", a);
+	void (*p)(int); // ponteiro para função que recebe int como parâmetro
+	p = signal(SIGINT, intHandler);
+	printf("Endereco do manipulador anterior %p\n", p);
+	p = signal(SIGQUIT, quitHandler);
+	printf("Endereco do manipulador anterior %p\n", p);
+	p = signal(SIGKILL, killhandler);
+	printf("Endereco do manipulador anterior %p\n", p);
+
+	puts("Ctrl-C desabilitado. Use Ctrl-\\ para terminar");
+	for (EVER);
+}
+void intHandler(int sinal)
+{
+	printf("Você pressionou Ctrl-C (%d)\n", sinal);
+	kill(0, SIGKILL);
+}
+void killhandler(int sinal)
+{
+	printf("Você matou o processo\n");
+}
+void quitHandler(int sinal)
+{
+	puts("Terminando o processo...");
+	exit(0);
+}
