6da49f2c7f81a1697f03f11f2bd699acb939b0d0
litaR84 <thalitarbullos@gmail.com>
2024-05-27 21:33:08 -0300

    Resolvido problema de exibição de mensagens do Processo 1

--- Lab5/sincronos.c
+++ Lab5/sincronos.c
@@ -16,36 +16,36 @@ int main(int argc, char *argv[]) {
     int segmento;
     pid_t pidProcesso1, pidProcesso2;
     char* mensagem;
-    sem_t *semafaro;
+    sem_t* semafaro;
     int status;
 
     // Alocar memória compartilhada
-    segmento = shmget (IPC_PRIVATE, MAX_TAM_MEN * sizeof(char), IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
+    segmento = shmget(IPC_PRIVATE, MAX_TAM_MEN * sizeof(char), IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
+
 
     // Associar a memória compartilhada ao processo
     mensagem = (char*)shmat(segmento, 0, 0);
-
-    // Criação do semáforo
     semafaro = sem_open("semafaro", O_CREAT, 0666, 0);
     sem_init(semafaro, 1, 1);
-    /* Processo 1 - Envio de mensagens */
-    // Duplicação do processo atual
+
     pidProcesso1 = fork();
 
     if (pidProcesso1 == 0) {
         char aux[MAX_TAM_MEN];
         // Envio de cada mensagem
-        for (int i = 0; i < QTD_MEN; i++) {
+        for (int i = 1; i <= QTD_MEN; i++) {
             sem_wait(semafaro); // Espera a leitura da mensagem
-            sprintf(aux, "mensagem %d", i + 1);
+            sprintf(aux, "mensagem %d", i);
             strcpy(mensagem, aux);
-            printf("Mensagem produzida: '%s'", mensagem);
+            printf("Processo 1: '%s'\n", mensagem);
             sem_post(semafaro); // Sinaliza o envio da mensagem
             sleep(1);
         }
+        
+        exit(0);
 
     } else if (pidProcesso1 < 0) {
-        printf("Erro ao criar Processo 1 para envio de mensagens.\n");
+        perror("Erro ao criar Processo 1 para envio de mensagens");
         exit(-1);
     }
 
@@ -56,14 +56,15 @@ int main(int argc, char *argv[]) {
     if (pidProcesso2 == 0) {
         for (int i = 1; i <= QTD_MEN; i++) {
             sem_wait(semafaro); // Espera até que o Processo 1 envie a mensagem
-
             printf("Processo 2: '%s'\n", mensagem);
-
             sem_post(semafaro); // Sinaliza que a mensagem foi lida
             sleep(1);
         }
+
+        exit(0);
+
     } else if (pidProcesso2 < 0) {
-        printf("Erro ao criar Processo 2 para leitura de mensagens.\n");
+        perror("Erro ao criar Processo 2 para leitura de mensagens");
         exit(-1);
     }
 
@@ -71,9 +72,9 @@ int main(int argc, char *argv[]) {
         wait(&status);
     }
 
-    /* Fechar semáforo e liberar memória compartilhada s*/
+    /* Fechar semáforo e liberar memória compartilhada */
     shmdt(mensagem);
-    sem_close(semafaro);
+    sem_destroy(semafaro);
     shmctl(segmento, IPC_RMID, NULL);
 
     return 0;
