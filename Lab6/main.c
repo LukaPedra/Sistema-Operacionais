293c5565bd3f7f05b078f4c9cf30a74e0fe5a909
Lucca Rocha <lucca.v.rocha@gmail.com>
2024-06-12 12:39:59 -0300

    update formatting

--- Lab6/main.c
+++ Lab6/main.c
@@ -14,32 +14,34 @@
 #define QTD_MEN 128
 #define TAM_BUFFER 8
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	key_t key;
 	int msgid;
-    pid_t pidProcesso1, pidProcesso2;
+	pid_t pidProcesso1;
 
 	key = ftok("key", 65);
 	msgid = msgget(key, 0666 | IPC_CREAT);
-    // Alocar memória compartilhada
-    //segmento = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
+	// Alocar memória compartilhada
+	// segmento = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
 
-
-    /* Processo 1 - Envio de mensagens */
-    pidProcesso1 = fork();
-	if (pidProcesso1 == -1) {
+	/* Processo 1 - Envio de mensagens */
+	pidProcesso1 = fork();
+	if (pidProcesso1 == -1)
+	{
 		printf("\nErro ao criar Processo 1.\n");
 		exit(1);
 	}
-	if (pidProcesso1 == 0) {
+	if (pidProcesso1 == 0)
+	{
 		// Enviar mensagem
 		process1_sync(msgid);
 	}
-    else  {
+	else
+	{
 		// Enviar mensagem
 		process2_sync(msgid);
-
 	}
-    
-    return 0;
+
+	return 0;
 }
--- Lab6/sincrono1.c
+++ Lab6/sincrono1.c
@@ -1,21 +1,25 @@
 #include "message.h"
 
-void process1_sync(int msgid) {
-    struct message msg;
-    for (int i = 1; i <= QTD_MEN; i++) {
-        msg.msg_type = 1;
-        msg.msg_data = i;
-        if (msgsnd(msgid, &msg, sizeof(msg.msg_data), 0) == -1) {
-            perror("msgsnd");
-            exit(1);
-        }
-        printf("Processo 1: Mensagem %d enviada\n", i);
+void process1_sync(int msgid)
+{
+	struct message msg;
+	for (int i = 1; i <= QTD_MEN; i++)
+	{
+		msg.msg_type = 1;
+		msg.msg_data = i;
+		if (msgsnd(msgid, &msg, sizeof(msg.msg_data), 0) == -1)
+		{
+			perror("msgsnd");
+			exit(1);
+		}
+		printf("Processo 1: Mensagem %d enviada\n", i);
 
-        // Receber a resposta
-        if (msgrcv(msgid, &msg, sizeof(msg.msg_data), 2, 0) == -1) {
-            perror("msgrcv");
-            exit(1);
-        }
-        printf("Processo 1: Resposta %d recebida\n", msg.msg_data);
-    }
+		// Receber a resposta
+		if (msgrcv(msgid, &msg, sizeof(msg.msg_data), 2, 0) == -1)
+		{
+			perror("msgrcv");
+			exit(1);
+		}
+		printf("Processo 1: Resposta %d recebida\n", msg.msg_data);
+	}
 }
